d181c6949bdcce88f953351c2e1779b1c9e6455b
Armando220497 <164913618+Armando220497@users.noreply.github.com>
2024-11-29 16:02:25 +0100

    update config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Aggiunge un percorso relativo per tutte le risorse
+  base: "./", // Mantieni il percorso relativo
+  build: {
+    outDir: "dist", // Directory di output per la build
+  },
+  server: {
+    open: true, // Apre il browser automaticamente
+    port: 3000, // Imposta la porta del server di sviluppo
+    // Importante per gestire le rotte di React Router durante lo sviluppo
+    historyApiFallback: true,
+  },
+  preview: {
+    port: 5000, // Porta per il server di anteprima
+    historyApiFallback: true, // Gestisce correttamente le rotte durante l'anteprima
+  },
 });
